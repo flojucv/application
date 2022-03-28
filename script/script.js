2d5711980d339e38a0055e0316dc601efb9a56e2
flojucv <flojucv@gmail.com>
2022-03-28 17:38:22 +0200

    update report.html & script.js

--- script/script.js
+++ script/script.js
@@ -9,7 +9,19 @@ catHtml.innerText = categorie;
 
 function envoieReport() {
     const nom = document.getElementById("nom");
-    if(nom.value){}
+    if(nom.value === "") return alert("Vous n'avez pas renseignez votre nom.");
+
+    const prenom = document.getElementById("prenom");
+    if(prenom.value === "") return alert("Vous n'avez pas renseignez votre prenom");
+
+    const email = document.getElementById('email');
+    if(email.value === "" || !email.value.includes("@")) return alert("Vous n'avez pas renseignez d'email valide");
+
+    const tel = document.getElementById('telephone');
+    if(tel.value === "" || tel.value.length != 10 || isNaN(tel.value)) return alert("Vous n'avez pas renseignez de numéro de téléphone valide.");
+
+    const message = document.getElementById("text-message");
+    if(message.value === "") return alert("Vous n'avez pas renseignez de message.");
 }
 
 const buton = document.getElementById("btn-envoyer");
