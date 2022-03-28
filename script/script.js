fc18d2c93856472956877dc15ea85b326d042469
flojucv <flojucv@gmail.com>
2022-03-28 17:19:12 +0200

    update

--- script/script.js
+++ script/script.js
@@ -1,4 +1,16 @@
 urlString = `${document.location.href}`;
 let categorie = urlString.split('?')[1];
+const catHtml = document.getElementById("categorie");
 
 document.title = `Report | ${categorie}`;
+catHtml.innerText = categorie;
+
+
+
+function envoieReport() {
+    const nom = document.getElementById("nom");
+    if(nom.value){}
+}
+
+const buton = document.getElementById("btn-envoyer");
+buton.addEventListener("click", envoieReport);
